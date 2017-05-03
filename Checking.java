ecf96698ccb89f8137349007cb04350358f81449
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 12:13:54 -0400

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,1 +1,14 @@
+/*
+ * Checking Class
+ *
+ * @author swiftycloudmusic
+ */
+package ist242team5bank;
 
+public class Checking
+{
+    public static void main(String[] args) 
+    {
+        
+    }    
+}
