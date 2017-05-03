1602878c6b0ef0d4eb4166973ad97818ee9a8249
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 11:31:16 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,1 +1,19 @@
+/*
+ * User Ability To Enter Info
+    - Firstname, Lastname
+ * Ability To Return String Representation Of The User 
+ * Store An ID Number (Random Number)
+ * Store Reference To 3 Account Instances (Checking, Savings, Vacation)
+ *
+ * @author swiftycloudmusic
+ */
+package ist242team5bank;
+
+public class IST242Team5Bank 
+{
+    public static void main(String[] args) 
+    {
+        
+    }    
+}
 
