76722cbdb036bb833a763261734b3366f996edc2
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 18:40:43 -0400

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -10,10 +10,82 @@
  */
 package ist242team5bank;
 
-public class Transaction
+// Import Methods
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Transaction 
 {
+    //Date Field
+    private String _date;
+    
+    // Call Date Field Method
+    public String Date() 
+    {
+        // Create New Date
+        Date date = new Date();
+
+        // Format Date Method
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        // Set Date Method
+        _date = (dateFormat.format(date));
+
+        // Return Date Method
+        return (_date);
+    }
+
+    // Main Class For Transaction
     public static void main(String[] args) 
     {
         
-    }    
+    }
+
+    // Report Deposit Or Withdrawal Transactions
+    public String Report() 
+    {
+        return "\nTransaction History\nDate and Time: " + date + "\nAccount: " 
+        + _accountType + "\nTransaction type: " + _transactionType + "\nAmount: " + _amount;
+    }
+    
+    // Report Transfer Information
+    public String ReportTransfer() 
+    {
+        return "\nTransfer History\nDate and Time: " + date + "\nSource account: "
+                + _accountType + "\nTarget account: " + _targetAccount
+                + "\nTransaction type: " + _transactionType
+                + "\nAmount: " + _amount;
+    }
+
+    // Class Level Variables
+    private String _transactionType = "";
+    private String _accountType = "";
+    private String _targetAccount = "";
+    private double _amount = 0;
+    private double _accountBalance = 0;
+    Date date = new Date();
+
+    // Instantiating A Deposit/Withdraw Transaction
+    public Transaction(String _accountType, String _transactionType, double _amount) 
+    {
+        // Values For Transaction Object
+        this.date = date;
+        this._accountType = _accountType;
+        this._accountBalance = _accountBalance;
+        this._transactionType = _transactionType;
+        this._amount = _amount;
+    }
+
+    // Instantiate Transfer 
+    public Transaction(String _accountType, String _targetAccount, String _transactionType, double _amount) {
+        //Assigning values revelant to Transaction object
+        this.date = date;
+        this._accountType = _accountType;
+        this._targetAccount = _targetAccount;
+        this._accountBalance = _accountBalance;
+        this._transactionType = _transactionType;
+        this._amount = _amount;
+    }
 }
