d0464723e89af4f8a1f2be4e1f825b4078e5e941
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 11:26:21 -0400

    Update IST242Team5Bank.java

--- IST242Team5Bank.java
+++ IST242Team5Bank.java
@@ -1,1 +1,19 @@
+/*
+ * Main Class Of Program.
+ */
+package ist242team5bank;
+/**
+ *
+ * @author swiftycloudmusic
+ */
+public class IST242Team5Bank 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }    
+}
 
