f7f46e798024a4ffafbb186eaa038850a296999c
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 12:14:37 -0400

    Update Vacation.java

--- Vacation.java
+++ Vacation.java
@@ -1,1 +1,14 @@
+/*
+ * Vacation Class
+ *
+ * @author swiftycloudmusic
+ */
+package ist242team5bank;
 
+public class Vacation
+{
+    public static void main(String[] args) 
+    {
+        
+    }    
+}
