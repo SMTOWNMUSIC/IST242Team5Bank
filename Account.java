b5c7a2fb42e6e2a4e944b4326cf363b6cf0a9b51
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 11:35:25 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,1 +1,20 @@
 
+/*
+ * Each Has A Type (Checking, Savings, Vacation
+ * Balance (Amount Of Money In Each Account)
+ * Ability To Do This For Any Account The User Chooses
+      * Deposit
+      * Withdraw 
+      * Transfer
+ * Will Utilize Transaction To Record Transactions
+ * @author swiftycloudmusic
+ */
+package ist242team5bank;
+
+public class IST242Team5Bank 
+{
+    public static void main(String[] args) 
+    {
+        
+    }    
+}
