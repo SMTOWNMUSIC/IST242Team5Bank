93127029c78532ab77ab5baaccdc2227d8a3da39
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 11:28:41 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,20 @@
+/*
+ * User Ability To Enter Info
+    - Firstname, Lastname
+ * Ability To Return String Representation Of The User 
+ * Store An ID Number (Random Number)
+ * Store Reference To 3 Account Instances (Checking, Savings, Vacation)
+ */
+package ist242team5bank;
+/**
+ *
+ * @author swiftycloudmusic
+ */
+public class IST242Team5Bank 
+{
+    public static void main(String[] args) 
+    {
+        
+    }    
+}
 
