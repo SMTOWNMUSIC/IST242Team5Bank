b3e204300a265573f25176afa9ecb9c582ddece4
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 21:52:04 -0400

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,12 +1,19 @@
 /*
  * Savings Class
- *
+ * Account Type Savings
+ * 
  * @author swiftycloudmusic
  */
 package ist242team5bank;
 
-public class Savings
+public class Savings extends Account
 {
+    public Savings (String _accountType)
+    {
+        super(_accountType);
+    }
+    
+    // Main Class For Savings 
     public static void main(String[] args) 
     {
         
