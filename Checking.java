27a6d8d129d64f49bf8b3879670a1e72ec67171d
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 21:50:20 -0400

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,12 +1,19 @@
 /*
  * Checking Class
+ * Account Type Is Checking
  *
  * @author swiftycloudmusic
  */
 package ist242team5bank;
 
-public class Checking
+public class Checking extends Account
 {
+    public Checking (String _accountType)
+    {
+        super(_accountType);
+    }
+    
+    // Main Class For Checking
     public static void main(String[] args) 
     {
         
