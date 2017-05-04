a608b7c53055768f157a243d5584852092a69c12
SMTOWNMUSIC <gpx5000@psu.edu>
2017-05-03 21:53:33 -0400

    Update Vacation.java

--- Vacation.java
+++ Vacation.java
@@ -1,12 +1,19 @@
 /*
  * Vacation Class
- *
+ * Account Type Vacation
+ * 
  * @author swiftycloudmusic
  */
 package ist242team5bank;
 
-public class Vacation
+public class Vacation extends Account
 {
+    public Vacation (String _accountType)
+    {
+        super(_accountType);
+    }
+    
+    // Main Class For Vacation
     public static void main(String[] args) 
     {
         
